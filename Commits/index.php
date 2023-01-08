7686487240b0c89aa1dd0985050a7ec4480285b0
rosa.zarco <zamr616@outlook.es>
2023-01-08 03:13:10 -0600

    style: Se agrega comentario
    Cambios que no afectan el significado del código

--- Commits/index.php
+++ Commits/index.php
@@ -3,5 +3,7 @@
 
     La especificación de Commits Convencionales es una convención ligera sobre los mensajes de commits. Proporciona un conjunto sencillo de reglas para crear un historial de commits explícito; lo que hace más fácil escribir herramientas automatizadas encima del historial.
 
+    #Se agrega comentario para prueba
+    Add
 
 ?>
