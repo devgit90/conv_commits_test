cc38a961cffd994a23eae6ceba7e5fc08355fea3
rosa.zarco <zamr616@outlook.es>
2023-01-08 03:02:07 -0600

    docs(index): Se agrega introducción
    El cuerpo es un mensaje mas detallado del commit
    
    BREAKING CHANGES: textxt

---
+++ Commits/index.php
@@ -0,0 +1,7 @@
+<?php
+    Prueba de Conventional Commits
+
+    La especificación de Commits Convencionales es una convención ligera sobre los mensajes de commits. Proporciona un conjunto sencillo de reglas para crear un historial de commits explícito; lo que hace más fácil escribir herramientas automatizadas encima del historial.
+
+
+?>
